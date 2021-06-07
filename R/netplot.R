b2c8f3f935f88ed1a4582092e5d442469faf8d7d
Tasnim <th19g983@eduroam-hg-dock-1-176.ethz.ch>
2021-06-07 12:02:22 +0200

    up

--- R/netplot.R
+++ R/netplot.R
@@ -13,12 +13,13 @@ prt.data.ad0 <- sapply(1:length(unique(prt.data$study)), function(i){
 }, simplify = F)
 prt.data.ad <- do.call(rbind,prt.data.ad0)
 all.data <- rbind(std.data[c('study','trt','outcome','n','design')],prt.data.ad)
-data.pair = pairwise(treat = trt, event = outcome, n = n,
-                          data = all.data, studlab = study, sm = "OR",warn = FALSE)
-net1 = netmeta(data.pair,warn = FALSE)
+data.pair = suppressWarnings(pairwise(treat = trt, event = outcome, n = n,
+                          data = all.data, studlab = study, sm = "OR",warn = FALSE))
+net1 = suppressWarnings(netmeta(data.pair,warn = FALSE))
 
 ## ------------------------------------------------------------------------
 return(netgraph(net1,offset = 0.1)) # ,col=1:net1$m col=as.numeric(all.data$design)
+
 }
 
 
